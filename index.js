7d2c1ffdf4a83bcffc58b2ba0a2989fab59f26ea
Abdifatah <abdifatah.101@outlook.com>
2021-08-28 02:09:33 +0100

    Added view & add via switch functions

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const consoleTable = require("console.table");
 //Connection for SQL DB
 const connection = mysql.createConnection({
     host: "localhost",
-    
+
     //Port
     port: 3306,
     //Username
@@ -16,32 +16,72 @@ const connection = mysql.createConnection({
     database: "Employee_Tracker.db",
 });
 //Connect to server and db
-connection.connect(function(err){
-    if(err) throw err;
+connection.connect(function (err) {
+    if (err) throw err;
     console.log("mySQL connected");
 
     start();
 });
+function start() {
+    inquirer.prompt([
+        {
+        type: 'list',
+        name: 'selection',
+        message: 'What would you like to do?',
+        choices: [
+            "View",
+            "Add",
+            "Update",
+            "Exit"
+        ]
+    }
 
-const menu = () =>
-  inquirer.prompt({
-    type: 'list',
-    name: 'selection',
-    message: 'What would you like to do?',
-    choices: [
-      'View employees',
-      'View employees by manager',
-      'Add employee',
-      'Update employee manager',
-      'Update employee role',
-      'Delete employee',
-      'View roles',
-      'Add role',
-      'Delete role',
-      'View departments',
-      'View department budget',
-      'Add department',
-      'Delete department',
-      'Exit',
-    ],
-  });
+    ]).then(function (res) {
+        switch (res.start) {
+            case "View":
+                view();
+                break;
+            case "Add":
+                add();
+                break;
+            case "Update":
+                updateEmployee();
+                break;
+            case "Exit":
+                console.log("All done")
+                break;
+            default:
+                console.log("default");
+                break;
+        }
+    })
+}
+
+function view(){
+    inquirer.prompt([
+        {
+            type: "list",
+            name: "view",
+            message: "Please select one of the following",
+            choices: ["All employees", "By department", "By role"]
+
+        }
+    ]).then(function(res){
+        switch (res.view) {
+            case "All employees":
+                viewAllEmployees();
+                
+                break;
+                case "By department":
+                    viewAllEmployees();
+                    
+                    break;
+                case "By role":
+                    viewByRole();
+        
+            default:
+                console.log("default");
+
+        }
+    });
+}
