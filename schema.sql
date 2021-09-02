aafa48b377af7eb37f53fce456eace3dbfc964a8
Abdifatah <abdifatah.101@outlook.com>
2021-09-02 16:00:12 +0100

    Added commas to schema

--- schema.sql
+++ schema.sql
@@ -3,8 +3,8 @@ CREATE DATABASE employee_db;
 
 USE employee_db;
 
-CREATE TABLE department (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE department(
+    id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30) UNIQUE NOT NULL
 );
 
@@ -25,11 +25,11 @@ CREATE TABLE employee(
     PRIMARY KEY (id)
 );
 
-INSTERT INTO department(name)
+INSERT INTO department(name)
 VALUES("HR"), ("Tech"), ("Admin"), ("Management");
 
 INSERT INTO role(title, salary, department_id)
-VALUES ("Director", 5700, 1), ("Manager", 4000, 2) ("Web Dev", 3000, 3);
+VALUES ("Director", 5700, 1), ("Manager", 4000, 2), ("Web Dev", 3000, 3);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES("John", "Smith", "Manager", 1) ("Daniel" "James", "Web Dev", 2)
